a3d18ff1279a5d4fa43da44780e263eb1af10391
Mailles Machiana <cortesmailles01@gmail.com>
2026-01-29 18:59:37 -0500

    fix(win): correct dev.ps1 wsl path for e-drive and add root npm scripts

--- dev.ps1
+++ dev.ps1
@@ -2,22 +2,24 @@
 # This script runs dev.sh inside WSL where Docker is installed
 
 param(
-    [Parameter(Position=0)]
+    [Parameter(Position = 0)]
     [string]$Mode = "",
     
-    [Parameter(Position=1)]
+    [Parameter(Position = 1)]
     [string]$SubCommand = ""
 )
 
-# Get the WSL path for the current directory
-$wslPath = "/mnt/d/signal-ops"
+# Get the WSL path for the current directory (Dynamic or E drive)
+$wslPath = "/mnt/e/signal-ops"
 
 # Build the command
 if ($SubCommand -ne "") {
     $command = "cd $wslPath && ./dev.sh $Mode $SubCommand"
-} elseif ($Mode -ne "") {
+}
+elseif ($Mode -ne "") {
     $command = "cd $wslPath && ./dev.sh $Mode"
-} else {
+}
+else {
     $command = "cd $wslPath && ./dev.sh"
 }
 
